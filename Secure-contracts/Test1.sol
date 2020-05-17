0202da89c82cdb0d05d595d0702b2f78f3b4f989
Mukesh Jaiswal <56516302+mukesh0000@users.noreply.github.com>
2020-05-17 13:38:21 +0530

    Add files via upload

---
+++ Secure-contracts/Test1.sol
@@ -0,0 +1,21 @@
+pragma solidity 0.6.0;
+
+contract FirstTest{
+  
+    uint data;
+    
+     constructor() public{
+       data =1;
+     }
+     function getData() public  view returns(uint){
+       return data;
+     }
+     function putData(uint _data) public{
+       data = _data;
+     }
+}
+
+
+// Report of Securify //
+
+/******   https://securify.chainsecurity.com/report/cbf1160b2131d4d91c0e087d5656767e8c8b0338e1246362b8286656a6dce2c9 ****/
