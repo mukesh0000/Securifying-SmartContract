c2336e012e44ab2507c3e2f9328dba4676828c36
Mukesh Jaiswal <56516302+mukesh0000@users.noreply.github.com>
2020-05-20 10:34:09 +0530

    Update Test1.sol

--- Secure-contracts/Test1.sol
+++ Secure-contracts/Test1.sol
@@ -21,3 +21,29 @@ contract FirstTest{
 /******   https://securify.chainsecurity.com/report/cbf1160b2131d4d91c0e087d5656767e8c8b0338e1246362b8286656a6dce2c9 ****/
 
 /** Contract After Rectification **/
+
+
+pragma solidity 0.6.0;
+
+contract FirstTest{
+  
+    uint data;
+    address owner;
+      
+    
+     constructor() public{
+       data =1;
+       owner = msg.sender;
+     }
+     modifier onlyowner(){
+       owner == msg.sender;
+       _;
+     }
+     function getData() public  view onlyowner returns(uint){
+       return data;
+     }
+     function putData(uint _data)  private {
+       require(_data > 0, "value is 0 or less than 0");
+       data = _data;
+     }
+}
